f0df54d48226c1a3844acb4baf1507bafd6addfc
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-22 16:24:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+class Polygon{
+    constructor(arr){
+        this.arr = arr;
+    }
+
+    get countSides(){
+        return this.arr.length;
+    }
+    
+    get perimeter(){
+        let count = 0;
+        for(let i = 0; i < this.arr.length; i++){
+            count += this.arr[i];
+        }
+        return count;
+    }
+}
